b2ab72cc3fc6f7654f700aa20b2e22d0a0a9d5c6
MaykonMorais <maykon.morais@protonmail.com>
2025-12-14 16:20:40 -0300

    feat: init project setup

---
+++ app-orders/src/http/server.ts
@@ -0,0 +1,66 @@
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+
+import { z } from "zod";
+
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { db } from "../db/client.ts";
+import { schema } from "../db/schema/index.ts";
+import { dispatchOrderCreated } from "../broker/messages/order-created.ts";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get("/health", async () => {
+  return { status: "OK" };
+});
+
+app.post(
+  "/orders",
+  {
+    schema: {
+      body: z.object({
+        amount: z.number(),
+        customerId: z.string(),
+      }),
+    },
+  },
+  async (request, response) => {
+    const { amount, customerId } = request.body;
+
+    console.log(`Received order for amount: ${amount}`);
+    const orderId = crypto.randomUUID();
+
+    const orderData = {
+      orderId,
+      amount,
+      customer: {
+        id: customerId,
+      },
+    };
+
+    dispatchOrderCreated(orderData);
+
+    await db.insert(schema.orders).values({
+      id: orderId,
+      customerId,
+      amount,
+    });
+
+    return response.status(201).send();
+  }
+);
+
+app.listen({ host: "0.0.0.0", port: 3333 }).then(() => {
+  console.log("[Orders] HTTP server running on http://localhost:3333");
+});
