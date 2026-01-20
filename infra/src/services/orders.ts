408e64d0e5e4c10da0f3332107925b7dfff59959
MaykonMorais <maykon.morais@protonmail.com>
2026-01-19 21:22:48 -0300

    feat: add load balancer and rabbitMQ

--- infra/src/services/orders.ts
+++ infra/src/services/orders.ts
@@ -1,6 +1,9 @@
 import * as awsx from "@pulumi/awsx";
-import { cluster } from "../services/cluster";
+import * as pulumi from "@pulumi/pulumi";
+
+import { cluster } from "../cluster";
 import { ordersDockerImage } from "../images/orders";
+import { rabbitMQAdminHttpListener, amqpListener } from "./rabbitmq";
 
 export const ordersService = new awsx.classic.ecs.FargateService(
   "fargate-orders",
@@ -13,7 +16,13 @@ export const ordersService = new awsx.classic.ecs.FargateService(
         image: ordersDockerImage.ref,
         cpu: 256,
         memory: 512,
+        environment: [
+          {
+            name: "BROKER_URL",
+            value: pulumi.interpolate`amqp://admin:admin@${amqpListener.endpoint.hostname}:${amqpListener.endpoint.port}`,
+          },
+        ],
       },
     },
-  }
+  },
 );
