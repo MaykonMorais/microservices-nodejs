4afd84e21ac5f5dda7c6c22c6e263d5cef1c92fe
MaykonMorais <maykon.morais@protonmail.com>
2026-01-24 21:23:52 -0300

    feat: add kong proxy listeners

--- infra/src/services/kong.ts
+++ infra/src/services/kong.ts
@@ -5,29 +5,96 @@ import { cluster } from "../cluster";
 import { appLoadBalancer, networkLoadBalancer } from "../load-balancer";
 
 import { kongDockerImage } from "../images/kong";
-
 import { ordersListener } from "../services/orders";
 
-export const rabbitMQService = new awsx.classic.ecs.FargateService(
-  "fargate-rabbitmq",
+// Proxy
+const proxyTargetGroup = appLoadBalancer.createTargetGroup(
+  "kong-proxy-target",
   {
-    cluster,
-    desiredCount: 1,
-    waitForSteadyState: false,
-    taskDefinitionArgs: {
-      container: {
-        image: kongDockerImage.ref,
-        cpu: 256,
-        memory: 512,
-        environment: [
-          { name: "KONG_DATABASE", value: "off" },
-          { name: "KONG_ADMIN_LISTEN", value: "0.0.0.0:8001" },
-          {
-            name: "ORDERS_SERVICE_URL",
-            value: pulumi.interpolate`http://${ordersListener.endpoint.hostname}:${ordersListener.endpoint.port}`,
-          },
-        ],
-      },
+    port: 8000,
+    protocol: "HTTP",
+    healthCheck: {
+      path: "/orders/health",
+      protocol: "HTTP",
     },
   },
 );
+
+const proxyHttpListener = appLoadBalancer.createListener(
+  "kong-proxy-listener",
+  {
+    port: 80,
+    protocol: "HTTP",
+    targetGroup: proxyTargetGroup,
+  },
+);
+
+// Admin UI
+const adminTargetGroup = appLoadBalancer.createTargetGroup(
+  "kong-admin-target",
+  {
+    port: 8002,
+    protocol: "HTTP",
+    healthCheck: {
+      path: "/",
+      protocol: "HTTP",
+    },
+  },
+);
+
+const adminHttpListener = appLoadBalancer.createListener(
+  "kong-admin-listener",
+  {
+    port: 8002,
+    protocol: "HTTP",
+    targetGroup: adminTargetGroup,
+  },
+);
+
+// Admin API
+const adminAPITargetGroup = appLoadBalancer.createTargetGroup(
+  "kong-admin-api-target",
+  {
+    port: 8001,
+    protocol: "HTTP",
+    healthCheck: {
+      path: "/",
+      protocol: "HTTP",
+    },
+  },
+);
+
+const adminAPIHttpListener = appLoadBalancer.createListener(
+  "kong-admin-api-listener",
+  {
+    port: 8001,
+    protocol: "HTTP",
+    targetGroup: adminAPITargetGroup,
+  },
+);
+
+export const kongService = new awsx.classic.ecs.FargateService("fargate-kong", {
+  cluster,
+  desiredCount: 1,
+  waitForSteadyState: false,
+  taskDefinitionArgs: {
+    container: {
+      image: kongDockerImage.ref,
+      cpu: 256,
+      memory: 512,
+      portMappings: [
+        proxyHttpListener,
+        adminHttpListener,
+        adminAPIHttpListener,
+      ],
+      environment: [
+        { name: "KONG_DATABASE", value: "off" },
+        { name: "KONG_ADMIN_LISTEN", value: "0.0.0.0:8001" },
+        {
+          name: "ORDERS_SERVICE_URL",
+          value: pulumi.interpolate`http://${ordersListener.endpoint.hostname}:${ordersListener.endpoint.port}`,
+        },
+      ],
+    },
+  },
+});
--- infra/src/services/orders.ts
+++ infra/src/services/orders.ts
@@ -3,7 +3,26 @@ import * as pulumi from "@pulumi/pulumi";
 
 import { cluster } from "../cluster";
 import { ordersDockerImage } from "../images/orders";
-import { rabbitMQAdminHttpListener, amqpListener } from "./rabbitmq";
+import { amqpListener } from "./rabbitmq";
+import { appLoadBalancer } from "../load-balancer";
+
+const ordersTargetGroup = appLoadBalancer.createTargetGroup("orders-target", {
+  port: 3333,
+  protocol: "HTTP",
+  healthCheck: {
+    path: "/health",
+    protocol: "HTTP",
+  },
+});
+
+export const ordersListener = appLoadBalancer.createListener(
+  "orders-listener",
+  {
+    port: 3333,
+    protocol: "HTTP",
+    targetGroup: ordersTargetGroup,
+  },
+);
 
 export const ordersService = new awsx.classic.ecs.FargateService(
   "fargate-orders",
@@ -16,10 +35,43 @@ export const ordersService = new awsx.classic.ecs.FargateService(
         image: ordersDockerImage.ref,
         cpu: 256,
         memory: 512,
+        portMappings: [ordersListener],
         environment: [
           {
             name: "BROKER_URL",
             value: pulumi.interpolate`amqp://admin:admin@${amqpListener.endpoint.hostname}:${amqpListener.endpoint.port}`,
+          },
+          {
+            name: "DATABASE_URL",
+            value: "",
+          },
+          {
+            name: "OTEL_SERVICE_NAME",
+            value: "orders",
+          },
+          {
+            name: "OTEL_TRACES_EXPORTER",
+            value: "otlp",
+          },
+          {
+            name: "OTEL_EXPORTER_OTLP_ENDPOINT",
+            value: "https://otlp-gateway-prod-sa-east-1.grafana.net/otlp",
+          },
+          {
+            name: "OTEL_EXPORTER_OTLP_HEADERS",
+            value: "",
+          },
+          {
+            name: "OTEL_RESOURCE_ATTRIBUTES",
+            value: "service.name=orders,service.namespace=nodejsgrafana",
+          },
+          {
+            name: "OTEL_NODE_RESOURCE_DETECTORS",
+            value: "env,host,os",
+          },
+          {
+            name: "OTEL_NODE_ENABLED_INSTRUMENTATIONS",
+            value: "http,https,fastify,pg,amqplib",
           },
         ],
       },
